fcc6608810840a6a926dd02cb4cae2733fbb427c
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2025-01-15 11:27:55 +0100

    inizializzato il progetto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TopHeader from './components/TopHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <TopHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TopHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopHeader = () => {
+    return (
+        <div>
+            <p className='text-6xl bg-blue-950 text-white m-10'>ciao</p>
+        </div>
+    )
+}
+
+export default TopHeader
